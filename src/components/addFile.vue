e74d8a8ee334eb3f4c6b8f663d8af3ef3edf874e
Amantatev Raiymbek <raiymbek.amanatyev@narxoz.kz>
2024-02-09 18:24:58 +0600

    new_commit

--- src/components/addFile.vue
+++ src/components/addFile.vue
@@ -7,8 +7,8 @@
     <label for="fileInput" class="custom-file-input-label">
       Choose Files
     </label>
-    <div v-for="(point, index) in points" :key="point.id">
-      <input type="checkbox" v-model="isSelected[index]" :value="point.code">{{ point.code }}/>
+    <div v-for="point in points" :key="point.id">
+      <input type="checkbox" v-model="isSelected[point.id]" :value="point.id">{{ point.code }}
     </div>
     <input type="file" id="fileInput" ref="fileInput" @change="handleFileChange" multiple />
     <button @click="uploadFiles()">Upload Files</button>
@@ -19,12 +19,12 @@
 
 import File from '../services/File'
 import Point from "@/services/Point";
-import Replic from "@/services/Replic";
+
 
   export default {
     data() {
       return {
-        isSelected: [],
+        isSelected: {},
         documentId: '',
         files: [],
         points: [],
@@ -35,7 +35,6 @@ import Replic from "@/services/Replic";
     async mounted() {
       const point = await Point.getPoints();
       this.points = point.data;
-
     },
     methods: {
       handleFileChange(e) {
@@ -43,66 +42,20 @@ import Replic from "@/services/Replic";
       },
       async uploadFiles() {
         try {
-          const fileIds = [];
-          const replicas = [];
           const formData = new FormData();
           formData.append('documentId', this.documentId);
 
           for (let i = 0; i < this.files.length; i++) {
             formData.append('files[]', this.files[i]);
           }
-
-          const response = await File.AddFile(formData);
-          const ids = response.data.map(item => item.id);
-
-          const checkCompressionInterval = setInterval(async () => {
-            const fileResponse = await File.getFile(ids);
-            const fileResponses = fileResponse.data;
-
-            // Check if any file is still not compressed
-            const notCompressedFiles = fileResponses.filter(file => file.compressed === 0);
-
-            if (notCompressedFiles.length > 0) {
-              console.log("Still waiting for compression...");
-            } else {
-              // If all files are compressed, stop the interval and continue with further actions
-              clearInterval(checkCompressionInterval);
-              console.log('Files upload successful:', response.data);
-
-              if (Array.isArray(response.data) && response.data.length > 0) {
-                for (let i = 0; i < response.data.length; i++) {
-                  const fileId = response.data[i].id;
-                  fileIds.push(fileId);
-                }
-                  console.log(fileIds)
-                  const selectedPoints = this.isSelected.reduce((acc, isSelected, index) => {
-                    if (isSelected) {
-                      acc.push(this.points[index].id);
-                    }
-                    return acc;
-                  }, []);
-
-                  console.log('Selected Points IDs:', selectedPoints);
-                  for (let i = 0; i < fileIds.length; i++) {
-                    for (let j = 0; j < selectedPoints.length; j++) {
-                      replicas.push({
-                        fileId: fileIds[i],
-                        pointId: selectedPoints[j],
-                      });
-                    }
-                  }
-                  const requestData = {
-                    replicas: replicas
-                  };
-                  console.log('Request Data:', requestData);
-                  const replicResponse = await Replic.AddRep(requestData);
-                  console.log(replicResponse.data)
-
-              } else {
-                console.error('Unexpected response structure:', response.data);
-              }
+          Object.keys(this.isSelected).forEach(pointId => {
+            if (this.isSelected[pointId]) {
+              formData.append('pointId', pointId);
             }
-          }, 1000);
+          });
+          console.log(this.isSelected)
+          const response = await File.AddFile(formData);
+          console.log(response.data)
         } catch (error) {
           console.error('Error uploading or replicating files:', error);
         }
